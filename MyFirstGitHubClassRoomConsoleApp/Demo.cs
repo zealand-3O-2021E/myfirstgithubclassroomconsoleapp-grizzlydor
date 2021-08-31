4b6c04def56417d9433e25455dc1034fb0113817
grizzlydor <artn2@inbox.lv>
2021-08-31 14:30:35 +0200

    My Commit \

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,14 @@
+﻿namespace MyFirstGitHubClassRoomConsoleApp
+{
+    public class Demo
+    {
+        public Demo(string text, string anotherText)
+        {
+            Text = text;
+            AnotherText = anotherText;
+        }
+        public string Text { get; set; }
+        public string AnotherText { get; set; }
+
+    }
+}
